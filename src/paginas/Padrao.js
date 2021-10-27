4a3d59b1acc626da3e8dd18601fc128b2e038c63
Lauro Tomiatti <lauro.tomiatti@docente.unip.br>
2021-10-26 21:44:06 -0300

    conexao com api

--- src/paginas/Padrao.js
+++ src/paginas/Padrao.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../servicos/api'
 
 function Padrao() {
+    const [restaurantes, setRestaurantes] = useState([])
+    useEffect(() => {
+        api.get('restaurantes/retornaTodos').then(resposta => {
+            setRestaurantes(resposta.data)
+        })
+    })
 
     return (
-        <h1> Página criada (existe!!) </h1>
+        <div className="row">
+            {
+                restaurantes.map(restaurante => {
+                    return (
+                        <div className="card" key={restaurante.id}>
+                            <h2> {restaurante.nome} </h2>
+                            <p> {restaurante.tipo} </p>
+                            <span> {restaurante.local} </span>
+                        </div>
+                    )
+                })
+            
+            }
+        </div>
     )
 }
 
